de11432c887d3f9d176d05f09f6f9cf792adf850
frederic.batault <frederic.batault@hotmail.fr>
2018-04-12 11:58:04 +0200

    lien entre les couches pour la fonctionnalité de connexion

--- Worspace/ProxibanqueBis/src/main/java/presentation/ConnexionServlet.java
+++ Worspace/ProxibanqueBis/src/main/java/presentation/ConnexionServlet.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import domaine.Conseiller;
+import service.ConseillerService;
 
 /**
  * Servlet implementation class ConnexionServlet
