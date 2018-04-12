5625ea6d802a5b905ae488bb360b3e031df3c17e
hedi-ines <hedi_ines@yahoo.fr>
2018-04-12 11:51:47 +0200

    ajout de package et des classe du Conseiller

---
+++ Worspace/ProxibanqueBis/src/main/java/dao/ConseillerDao.java
@@ -0,0 +1,55 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import domaine.Conseiller;
+
+public class ConseillerDao {
+
+	public Conseiller lecture (String log) {
+		// ETAPE1: chargement du driver
+
+		System.out.println("Chargement du Driver");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Etape 2 : connexion DB
+
+			Connection maConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Proxibanque", "root", "");
+
+			// Etape 3: preparation de la requete
+			Statement stm = maConnection.createStatement();
+			String s = "SELECT * FROM Conseiller WHERE login  =" + log;
+
+			// execution d'une selection
+			ResultSet rs = stm.executeQuery(s);
+			
+			// Etape 5 : lecture des données lecture d'une selection
+						
+			rs.next();
+			int idConseiller = rs.getInt("idConseiller");
+			String nom =rs.getString("nom");
+			String prenom =rs.getString("prenom");
+			String login =rs.getString("login");
+			String password =rs.getString("password");		
+			Conseiller conseillerReturn = new Conseiller(idConseiller, nom, prenom, login, password);
+			return conseillerReturn; 
+
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+			return null;
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			return null;
+		}
+		
+
+	}
+
+}
---
+++ Worspace/ProxibanqueBis/src/main/java/service/ConseillerService.java
@@ -0,0 +1,28 @@
+package service;
+
+import dao.ConseillerDao;
+import domaine.Conseiller;
+
+public class ConseillerService {
+		
+		
+		public boolean authentification (String login, String password1) {
+			ConseillerDao infoConseiller = new ConseillerDao();
+		Conseiller conseiller = infoConseiller.lecture(login);
+		String password2=conseiller.getLogin();
+		
+		if( password1==password2) {
+			return true;
+
+}
+		else {
+			return false;
+		}
+	}
+		
+		public Conseiller lecture(String login) {
+			ConseillerDao infoConseiller = new ConseillerDao();
+			Conseiller conseillerRetour= infoConseiller.lecture(login);
+			return conseillerRetour;
+		}
+}
