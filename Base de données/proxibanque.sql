21a859fe392fdadb3632bf9a8ab1033a40c9ef3b
hedi-ines <hedi_ines@yahoo.fr>
2018-04-12 10:25:06 +0200

    modification base de donne

--- Base de données/proxibanque.sql
+++ Base de données/proxibanque.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 11 avr. 2018 à 20:38
--- Version du serveur :  5.7.19
--- Version de PHP :  5.6.31
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 12 Avril 2018 à 08:17
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `proxibanque`
 --
-CREATE DATABASE IF NOT EXISTS `proxibanque` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `proxibanque`;
 
 -- --------------------------------------------------------
 
@@ -30,16 +26,30 @@ USE `proxibanque`;
 -- Structure de la table `client`
 --
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
-  `idClient` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `client` (
+  `idClient` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `adresse` varchar(255) NOT NULL,
   `codepostale` varchar(255) NOT NULL,
   `ville` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`idClient`)
+  `idConseiller` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+CREATE TABLE `compte` (
+  `idCompte` int(11) NOT NULL,
+  `solde` double NOT NULL,
+  `typeCompte` varchar(255) NOT NULL DEFAULT 'CompteCourant',
+  `decourvertAutorise` double DEFAULT NULL,
+  `tauxRemuneration` double DEFAULT NULL,
+  `idClient` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -48,17 +58,67 @@ CREATE TABLE IF NOT EXISTS `client` (
 -- Structure de la table `conseillerclient`
 --
 
-DROP TABLE IF EXISTS `conseillerclient`;
-CREATE TABLE IF NOT EXISTS `conseillerclient` (
-  `IdConseiller` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `conseillerclient` (
+  `IdConseiller` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`IdConseiller`)
+  `password` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
 
+--
+-- Contenu de la table `conseillerclient`
+--
+
+INSERT INTO `conseillerclient` (`IdConseiller`, `nom`, `prenom`, `login`, `password`) VALUES
+(1, 'Mbiandou', 'Douglas', 'douglas.mbiandou', 'Objis.java'),
+(2, 'Pean', 'Frederic', 'frediric.pean', 'Gtm.formation'),
+(3, 'Mhoumad', 'Nassur', 'nassur.mhounad', 'Objis.ajile'),
+(4, 'Azzouz', 'Lotfi', 'lotfi.azzouz', 'Gtm.proxibanque');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`idClient`),
+  ADD KEY `fk_conseillerClient` (`idConseiller`);
+
+--
+-- Index pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`idCompte`),
+  ADD KEY `fk_client` (`idClient`);
+
+--
+-- Index pour la table `conseillerclient`
+--
+ALTER TABLE `conseillerclient`
+  ADD PRIMARY KEY (`IdConseiller`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `compte`
+--
+ALTER TABLE `compte`
+  MODIFY `idCompte` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `conseillerclient`
+--
+ALTER TABLE `conseillerclient`
+  MODIFY `IdConseiller` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
